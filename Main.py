ce1f9a28f508881e65c035de1fa72cc645d3c3b8
Federic Milone <fedeemilo91@gmail.com>
2019-06-29 02:23:05 -0300

    Módulo principal de inicio

--- Main.py
+++ Main.py
@@ -3,6 +3,13 @@ from jugar import jugar
 from configuracion import configuracion
 sg.ChangeLookAndFeel('BrownBlue')
 layout = [    
+          [sg.T('  Sopa de Letras \n     Educativa   ', 
+            background_color='lightgreen', 
+            text_color='black',
+            relief='groove',
+            auto_size_text=True,
+            size=(29,2),
+            font=('Courier', 18, 'bold'))],
           [sg.Button("Jugar",
             size=(20,3),
             button_color=("white","green"), 
@@ -16,12 +23,17 @@ layout = [
             border_width=20)],
           [sg.Button("Salir",
             size=(20,3),
-            button_color=("white","green"), 
+            button_color=("white","red"), 
             pad=(15,10), 
             border_width=20)],
          ]
 #VENTANA PRINCIPAL
-window_main = sg.Window("Juego Educativo",size=(290,375), font='Courier').Layout(layout)
+window_main = sg.Window("Juego Educativo",
+              size=(320,455), 
+              font='Courier', 
+              background_color='lightgreen',
+              no_titlebar=True,
+              grab_anywhere=True).Layout(layout)
 
 while True:
  button, values = window_main.Read()
