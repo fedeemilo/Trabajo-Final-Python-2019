1812219b72ffccb42357a3e0d238cb59df5ce3a7
fedeemilo <fedeemilo91@gmail.com>
2019-08-06 01:37:39 -0300

    modularizando..

---
+++ muestra_datos.py
@@ -0,0 +1,108 @@
+import time
+from itertools import repeat
+from luma.core import legacy
+### MATRIZ
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT,TINY_FONT, SINCLAIR_FONT, LCD_FONT
+### MIC
+import RPi.GPIO as GPIO
+#para emular
+from demo_opts import get_device
+class Matriz:
+	def __init__(self, numero_matrices = 1, orientacion = 0, rotacion = 0, ancho = 8, alto = 8):
+		self.font = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
+		self.serial = spi(port=0, device=0, gpio=noop())
+		self.device = max7219(self.serial, width = ancho, height = alto, cascaded = numero_matrices, rotate = rotacion)
+	def mostrar_mensaje(self, msg, delay=0.1, font=1):
+		show_message(self.device, msg, fill = 'white', font = proportional(self.font[font]), scroll_delay=delay)
+
+
+class Sonido:
+    def __init__(self, canal=22):
+         """Init para la configuración del mic"""
+            self._canal = canal
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(self._canal, GPIO.IN)
+         # Desactivo las warnings por tener más de un circuito en la GPIO
+            GPIO.setwarnings(False)
+            GPIO.add_event_detect(self._canal, GPIO.RISING)
+
+    def evento_detectado(self, mostrarM):
+         """Si se detecta un sonido (aplauso) se muestran los datos de temperatura y humedad en la matriz"""
+            if GPIO.event_detected(self._canal):
+                mostrarM()
+def main():
+    emu = input('Está emulando? (y or n): ').lower() in ('s','y','si','yes')
+    #emu = True
+    if emu:
+        mostrarEnMatriz(emu)
+    else:
+        iniciar_sensores()
+        sonido = Sonido()
+        while True:
+            time.sleep(0.0001)
+            sonido.evento_detectado(mostrarEnMatriz)                      
+
+def mostrarEnMatriz(emu):
+"""Módulo para mostrar los datos de temperatura y humedad en la matriz"""
+    #si el dispositivo es un emulador
+	if emu: #para emular pido el device
+		device = get_device()
+		datos = {'temperatura':34, 'humedad':89}
+	else:
+		#Inicializar la matriz: identificar puerto
+		serial = spi(port = 0, device = 0, gpio = noop())
+		#crear una insctancia del objeto (matriz)
+		device = max7219(serial, cascaded = 2, block_orientation = 0)
+    
+    #abro el archivo datos_oficinas creado en la app registro_ambiental y guardo
+    #el último registro de temperatura y humedad registrado en el número de 
+    # oficina que ingresó el usuario
+    with open('datos_oficinas.json', "r") as archivo:
+        try:
+            datos_registro = json.load(archivo)
+
+    temp = datos_registro["oficina"+str(num)][len(datos_registro["oficina"+str(num)])]["temperatura"]
+    hum = datos_registro["oficina"+str(num)][len(datos_registro["oficina"+str(num)])]["humedad"]
+
+	temp = datos['temperatura']
+	hum = datos['humedad']
+    #--------------------------------------------------------------------------------------
+    #muestro primero la palabra Temperatura y luego con un delay 
+    msg = 'Temperatura'
+	show_message(device, msg, fill='white', font=proportional(LCD_FONT), scroll_delay=0.05)
+	msg = str(temp)+'º C'
+	with canvas(device) as draw:
+		text(draw, (1, 0), msg, fill="white")
+	time.sleep(3)
+	msg = 'Humedad'
+	show_message(device, msg, fill='white', font=proportional(LCD_FONT), scroll_delay=0.05)
+	msg = str(hum)+'%'
+	with canvas(device) as draw:
+		text(draw, (1, 0), msg, fill="white")
+	time.sleep(3)
+    #--------------------------------------------------------------------------------------
+    #--------------------------------------------------------------------------------------
+if __name__ == "__main__":
+  num=input("¿En que oficina se encuentra?(el numero solamente)")
+  try:
+   main()
+  except KeyboardInterrupt:
+    pass #no hacer nada              
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ registro_ambiental.py
@@ -0,0 +1,48 @@
+import os
+import json
+import time
+import Adafruit_DHT
+##############################################################################################
+#Creo una clase que tiene una funcion que obtiene temperatura/humedad y la devuelve como diccionario.
+class Temperatura_Humedad:
+    def __init__(self, pin=17, sensor=Adafruit_DHT.DHT11):
+        # Usamos el DHT11 que es compatible con el DHT12
+        # en caso de usar el 22 Adafruit_DHT.DHT22
+        self._sensor = sensor
+        self._data_pin = pin
+
+    def datos_sensor(self):
+        humedad, temperatura = Adafruit_DHT.read_retry(self._sensor, self._data_pin)
+        return {'temperatura': temperatura, 'humedad': humedad}
+##############################################################################################
+#Creo un objeto de la clase ya mencionada.
+objeto = Temperatura_Humedad()
+##############################################################################################
+#La funcion llama al objeto creado para obtener la informacion y colocarla en la variable "informacion", ademas se le coloca la fecha de registro.
+def leer_informacion():
+    informacion= Temperatura_Humedad.datos_sensor()
+    informacion.update({"fecha": time.asctime(time.localtime(time.time()))})
+    return informacion
+############################################################################################## 
+#La funcion guarda los datos obtenidos por info en el archivo json "datos_oficinas.json" , si se encuentra vacio crea la lista para llenarla con los datos.
+def guardar_informacion(info,num):
+    with open(datos_oficinas.json, "r") as archivo:
+        try:
+            datos_registro = json.load(archivo)
+        except Exception:
+            datos_registro = {}
+    try
+     datos_registro["oficina"+str(num)].append(info)
+    except Exception:
+     datos_registro["oficina"+str(num)]=[]
+    with open(datos_oficinas.json, "w") as archivo:
+        json.dump(datos_registro, archivo, indent=4)
+##############################################################################################
+#Primero se selecciona en que oficina se encuentra.
+#Luego ,cada  1 minuto se obtiene datos y se lo colocan en el archivo json "datos_oficinas.json" 
+if __name__ == "__main__":
+	num = input("¿En que oficina se encuentra?(sólo el número)")
+    while True:
+        dato =leer_informacion()
+        guardar_informacion(dato,num)
+        time.sleep(60) 
